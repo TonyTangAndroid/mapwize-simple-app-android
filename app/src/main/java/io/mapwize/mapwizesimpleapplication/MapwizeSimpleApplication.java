559f172693c1214561211146331cb6cc2e056649
Etienne Mercier <etienne@mapwize.io>
2018-05-07 11:23:20 +0200

    Update mapwize to 1.3.2

--- app/src/main/java/io/mapwize/mapwizesimpleapplication/MapwizeSimpleApplication.java
+++ app/src/main/java/io/mapwize/mapwizesimpleapplication/MapwizeSimpleApplication.java
@@ -9,7 +9,7 @@ public class MapwizeSimpleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        AccountManager.start(this, "1f04d780dc30b774c0c10f53e3c7d4ea"); // PASTE YOU MAPWIZE API KEY HERE !!! This is a demo key, giving you access to the demo building. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signin up at mapwize.io
+        AccountManager.start(this, "YOUR_MAPWIZE_API_KEY"); // PASTE YOU MAPWIZE API KEY HERE !!! Get your key by signin up at mapwize.io
     }
 
 }
